a9b9b05bc4e7991a54b805276f0120e98efcaad3
Rong L <rong.lang@slu.se>
2025-08-29 13:35:06 +0200

    commented out loading library

--- app.R
+++ app.R
@@ -1,8 +1,8 @@
 #install.packages("rsconnect")
-library(rsconnect)
+# library(rsconnect) #comment out after deploy the app, as it cause runtime error in publish it on cloud 
 rsconnect::setAccountInfo(name='rong0001', token='A31BFAAB3A1BD63AD1C93941CC41C5DF', secret='tdLPYIIzjtUMWjRQXdgYlC7UTWdNg86O/9dB+JEF')
 
 # once rsconnect is configured, deploy you shiny app
 rsconnect::deployApp("Exercise_1")
 
-rsconnect::writeManifest()
+#rsconnect::writeManifest() # run after deploy the app to create the dependency file
