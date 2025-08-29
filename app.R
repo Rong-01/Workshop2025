c17d06d6740c5dcb83c049fbe55e2825e8caf2fd
Rong L <rong.lang@slu.se>
2025-08-29 12:14:17 +0200

    Another try on 29 Aug

---
+++ app.R
@@ -0,0 +1,8 @@
+#install.packages("rsconnect")
+library(rsconnect)
+rsconnect::setAccountInfo(name='rong0001', token='A31BFAAB3A1BD63AD1C93941CC41C5DF', secret='tdLPYIIzjtUMWjRQXdgYlC7UTWdNg86O/9dB+JEF')
+
+# once rsconnect is configured, deploy you shiny app
+rsconnect::deployApp("Exercise_1")
+
+rsconnect::writeManifest()
