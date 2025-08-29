89175852bc4b10911777996a4916640b7520cd90
Rong L <rong.lang@slu.se>
2025-08-29 17:02:11 +0200

    update by adding ui and serer

--- app.R
+++ app.R
@@ -1,8 +1,19 @@
 #install.packages("rsconnect")
-# library(rsconnect) #comment out after deploy the app, as it cause runtime error in publish it on cloud 
-rsconnect::setAccountInfo(name='rong0001', token='A31BFAAB3A1BD63AD1C93941CC41C5DF', secret='tdLPYIIzjtUMWjRQXdgYlC7UTWdNg86O/9dB+JEF')
+#install.packages("shiny")
+#library(shiny)
+#install.packages(c('ggplot2','Hmisc'))
+#library(Hmisc)
 
-# once rsconnect is configured, deploy you shiny app
+#congiure rsconnect
+rsconnect::setAccountInfo(name='rong0001', token='0FC5C9FADEE8615C6F0C06F8AF0A0D39', secret='0uUnmrf1xlY6Y+rTqmJuwfXFz/x1SsTgVTf+tHYa')
+
+#deploy app
+#library(rsconnect)
 rsconnect::deployApp("Exercise_1")
+# why do I get this message while I am demploying exercise 1???
+# e-deploying "exercise_2" using "server: shinyapps.io / username: rong0001"
+# ℹ Looking up application with id "15302302"...✔ Found application <https://rong0001.shinyapps.io/exercise_2/>
+shinyApp(ui = ui, server = server)
 
-#rsconnect::writeManifest() # run after deploy the app to create the dependency file
+# create dependency file after running the deployment
+rsconnect::writeManifest()
