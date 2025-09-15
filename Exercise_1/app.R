c35fe324a4e9a938f21eabd1df6d636b31cbf8c5
Rong L <rong.lang@slu.se>
2025-09-15 10:25:41 +0200

    2025.09.15

---
+++ Exercise_1/app.R
@@ -0,0 +1,11 @@
+# app.R for exercise 1
+
+source("ui.R")
+source("server.R")
+
+shinyApp(ui = ui, server = server)
+
+library(rsconnect) #comment out after deploy the app, as it cause runtime error in publish it on cloud 
+
+rsconnect::deployApp("Exercise_1")
+
