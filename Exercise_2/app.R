be66941f0456f05c7a013e42fa8e227f76c60387
Rong L <rong.lang@slu.se>
2025-09-15 14:08:15 +0200

    Exercise 2 updated

--- Exercise_2/app.R
+++ Exercise_2/app.R
@@ -9,3 +9,7 @@ library(rsconnect) #comment out after deploy the app, as it cause runtime error 
 
 rsconnect::deployApp("Exercise_2")
 
+# run the following two lines in the console to generate the dependency file in the same directory of main applicaiton
+# setwd("Exercise_2")
+# rsconnect::writeManifest() # if it shows unable to change then use the following line with explicit path
+# rsconnect::writeManifest(appDir = "C:/MyTemp/Workshop_exercise2025/Workshop2025/Exercise_2")
