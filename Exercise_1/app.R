486dd20144ed009622c00a2bcffae8926902a27a
Rong L <rong.lang@slu.se>
2025-09-15 13:49:47 +0200

    Generated dependency file in the main application directory

--- Exercise_1/app.R
+++ Exercise_1/app.R
@@ -9,3 +9,7 @@ library(rsconnect) #comment out after deploy the app, as it cause runtime error 
 
 rsconnect::deployApp("Exercise_1")
 
+# run the following two lines in the console to generate the dependency file in the same directory of main applicaiton
+# setwd("Exercise_1")
+# rsconnect::writeManifest()
+  
